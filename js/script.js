cbbbffcbbcf31bebc9dac9c486d9f7b748b75a63
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-06-03 13:56:42 -0400

    Minor changes

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
     $(".js--header").waypoint(function(direction) {
         $(".js--header").addClass("animated zoomIn");
     }, {
-        offset: "50%"
+        offset: "65%"
     });
 
     $(".js--text-1").waypoint(function(direction) {
         $(".js--text-1").addClass("rotateInDownRight").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
     });
 
     $(".js--header-2").waypoint(function(direction) {
         $(".js--header-2").addClass("rotateInDownLeft").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
     });
 
     $(".js--pic-1").waypoint(function(direction) {
@@ -27,52 +27,56 @@ $(document).ready(function() {
     $(".js--text-2").waypoint(function(direction) {
         $(".js--text-2").addClass("fadeIn").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
     });
 
     $(".js--pic-2").waypoint(function(direction) {
         $(".js--pic-2").addClass("fadeInLeft").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
     });    
 
     $(".js--pic-3").waypoint(function(direction) {
         $(".js--pic-3").addClass("fadeInRight").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
     });  
 
     $(".js--pic-4").waypoint(function(direction) {
         $(".js--pic-4").addClass("jackInTheBox").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
     });  
     
     $(".js--pic-5").waypoint(function(direction) {
         $(".js--pic-5").addClass("fadeInDownBig").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
     });  
     
     $(".js--pic-6").waypoint(function(direction) {
-        $(".js--pic-6").addClass("fadeInDownBig").css("opacity", "1");
+        $(".js--pic-6").addClass("fadeInUpBig").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
   
     });  
 
     $(".js--pic-7").waypoint(function(direction) {
         $(".js--pic-7").addClass("fadeInDownBig").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
     });  
 
     $(".js--pic-8").waypoint(function(direction) {
-        $(".js--pic-8").addClass("fadeInDownBig").css("opacity", "1");
+        $(".js--pic-8").addClass("fadeInUpBig").css("opacity", "1");
     }, {
-        offset: "50%"
+        offset: "60%"
     });  
 
-    // Change animation for showcase
+    $(".js--pic-9").waypoint(function(direction) {
+        $(".js--pic-9").addClass("SlideInRight").css("opacity", "1");
+    }, {
+        offset: "55%"
+    });  
 
 });
